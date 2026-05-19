15b61f947cbd961ae8e1b0f58d23d4732bcabd6c
Igor Negherbon Heiser <igor_heiser@estudante.sesisenai.org.br>
2026-05-19 16:25:30 -0300

    feat(aluno): cria entity aluno

---
+++ src/model/entity/Aluno.java
@@ -0,0 +1,45 @@
+package model.entity;
+
+public class Aluno {
+
+    private int id;
+    private int idPlus = 1;
+    private String nome;
+    private String matricula;
+
+    public Aluno(String nome, String matricula) {
+        this.id = idPlus++;
+        this.nome = nome;
+        this.matricula = matricula;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString(){
+        return "\nInforamações do aluno: \nID: " + id + "\nNome: " + "\nMatrícula" + matricula;
+    }
+
+}
