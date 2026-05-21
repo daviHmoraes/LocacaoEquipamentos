ef16992cff7bd1f251828d21f6429fad21ef2eef
Arthur Jardim <arthur_jardim-souza@estudante.sesisenai.org.br>
2026-05-21 18:26:18 -0300

    style(equipamento): ajusta identação da classe EquipamentoRepository

--- src/model/repository/EquipamentoRepository.java
+++ src/model/repository/EquipamentoRepository.java
@@ -6,44 +6,43 @@ import java.util.HashMap;
 
 public class EquipamentoRepository {
 
-private HashMap<Integer, Equipamento> equipamentoHashMap;
+    private HashMap<Integer, Equipamento> equipamentoHashMap;
 
 
-public EquipamentoRepository(){
+    public EquipamentoRepository() {
 
-    equipamentoHashMap = new HashMap<>();
+        equipamentoHashMap = new HashMap<>();
 
-}
+    }
 
-public Equipamento buscarID(int id){
+    public Equipamento buscarID(int id) {
 
-    return equipamentoHashMap.get(id);
+        return equipamentoHashMap.get(id);
 
-}
+    }
 
-public  void salvar (Equipamento equipamento){
+    public void salvar(Equipamento equipamento) {
 
-    equipamentoHashMap.put(equipamento.getId(), equipamento);
+        equipamentoHashMap.put(equipamento.getId(), equipamento);
 
-}
+    }
 
-public HashMap<Integer, Equipamento> listarEquipamento(){
+    public HashMap<Integer, Equipamento> listarEquipamento() {
 
-    return equipamentoHashMap;
+        return equipamentoHashMap;
 
-}
+    }
 
-public void removerEquipamento(int id){
+    public void removerEquipamento(int id) {
 
-    equipamentoHashMap.remove(id);
+        equipamentoHashMap.remove(id);
 
-}
+    }
 
-public void atualizar (Equipamento equipamento){
+    public void atualizar(Equipamento equipamento) {
 
-equipamentoHashMap.put(equipamento.getId(), equipamento);
+        equipamentoHashMap.put(equipamento.getId(), equipamento);
 
-
-}
+    }
 
 }
