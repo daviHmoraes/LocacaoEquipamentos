00d6b661bd69551c82b46ac8387a1bc84b0cc8ff
Arthur Jardim <arthur_jardim-souza@estudante.sesisenai.org.br>
2026-05-22 16:48:25 -0300

    style(equipamento): reidentação de métodos EquipamentoRepository

--- src/model/repository/EquipamentoRepository.java
+++ src/model/repository/EquipamentoRepository.java
@@ -6,43 +6,27 @@ import java.util.HashMap;
 
 public class EquipamentoRepository {
 
-    private HashMap<Integer, Equipamento> equipamentoHashMap;
+    private HashMap<Integer, Equipamento> equipamentoHashMap = new HashMap<>();
 
-
-    public EquipamentoRepository() {
-
-        equipamentoHashMap = new HashMap<>();
-
-    }
-
+    //buscar ID
     public Equipamento buscarID(int id) {
-
         return equipamentoHashMap.get(id);
-
     }
-
+    //salvar
     public void salvar(Equipamento equipamento) {
-
         equipamentoHashMap.put(equipamento.getId(), equipamento);
-
     }
-
+    //listar
     public HashMap<Integer, Equipamento> listarEquipamento() {
-
         return equipamentoHashMap;
-
     }
-
+    //remover
     public void removerEquipamento(int id) {
-
         equipamentoHashMap.remove(id);
-
     }
-
+    //atualizar
     public void atualizar(Equipamento equipamento) {
-
         equipamentoHashMap.put(equipamento.getId(), equipamento);
-
     }
 
 }
